67d842588f89bd89bb4828034fd05ce804490af8
Angel Badillo Hernandez <badilloa022402@gmail.com>
2023-09-28 20:02:21 -0500

    type annotations in sqliteCRUD.py

--- sqliteCRUD.py
+++ sqliteCRUD.py
@@ -2,57 +2,59 @@
 
 import sqlite3
 from prettytable import PrettyTable
-   
+
+
 class SQLiteconn:
-    
-    def __init__(self, db_path):
+    def __init__(self, db_path: str) -> None:
         """Initialize database connection and cursor."""
         self.db_path = db_path
         self.conn = sqlite3.connect(self.db_path)
         self.cursor = self.conn.cursor()
 
-    def __raw_results(self, results):
+    def __raw_results(self, results) -> list[str]:
         """Convert raw results to a list of table names."""
         table = []
         for row in results:
             table.append(row[0])
-        return table     
+        return table
 
-    def __formatted_results(self, results):
+    def __formatted_results(self, results) -> PrettyTable:
         """Format results as a PrettyTable."""
         table = PrettyTable()
         table.field_names = [desc[0] for desc in self.cursor.description]
         table.add_rows(results)
         return table
 
-    def create_table(self, table_name, columns):
+    def create_table(self, table_name:str, columns:list) -> None:
         """
         Params:
             table_name (str) - name of table
             columns (list) - ["id INTEGER PRIMARY KEY", "name TEXT", "created TEXT", "modified TEXT", "size REAL","type TEXT","owner TEXT","owner_group TEXT","permissions TEXT"]
 
         Create a new table with specified columns.
-        
+
         Args:
             table_name (str): Name of the table.
             columns (list): List of column definitions.
         """
         try:
             # Create a table with the given columns
-            create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)});"
+            create_table_query:str = (
+                f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)});"
+            )
             self.cursor.execute(create_table_query)
             self.conn.commit()
             print(f"Table '{table_name}' created successfully.")
         except sqlite3.Error as e:
             print(f"Error creating table: {e}")
-            
-    def drop_table(self, table_name):
+
+    def drop_table(self, table_name:str) -> None:
         """
         Params:
             table_name (str) - name of table
             columns (list) - ["id INTEGER PRIMARY KEY", "name TEXT", "created TEXT", "modified TEXT", "size REAL","type TEXT","owner TEXT","owner_group TEXT","permissions TEXT"]
         Drop a table by name.
-        
+
         Args:
             table_name (str): Name of the table to drop.
         """
@@ -64,26 +66,24 @@ class SQLiteconn:
             print(f"Dropped '{table_name} successfully.")
         except sqlite3.Error as e:
             print(f"Error creating table: {e}")
-            
 
-    def show_tables(self,raw=True):
+    def show_tables(self, raw:bool=True) -> list[str]:
         """Show all tables in the database.
-        
+
         Args:
             raw (bool): Whether to return raw results or formatted table.
         """
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         results = self.cursor.fetchall()
 
-        if not raw: 
+        if not raw:
             return self.__formatted_results(results)
         else:
             return self.__raw_results(results)
 
-
-    def describe_table(self,table_name,raw=False):
+    def describe_table(self, table_name:str, raw:bool=False) -> PrettyTable | list[dict]:
         """Describe the structure of a table.
-        
+
         Args:
             table_name (str): Name of the table.
             raw (bool): Whether to return raw data or a PrettyTable.
@@ -91,34 +91,38 @@ class SQLiteconn:
         self.cursor.execute(f"PRAGMA table_info({table_name});")
         results = self.cursor.fetchall()
         table = None
-        
+
         if not raw:
             table = self.__formatted_results(results)
-           
+
         else:
-        
-            table= []
+            table = []
 
             for column_info in results:
                 column_name = column_info[1]
                 data_type = column_info[2]
                 is_nullable = "NULL" if column_info[3] == 0 else "NOT NULL"
-                table.append({"column_name":column_name,"data_type":data_type,"isnull":is_nullable})
-                #print(f"Column Name: {column_name}, Data Type: {data_type}, Nullable: {is_nullable}")
-                
+                table.append(
+                    {
+                        "column_name": column_name,
+                        "data_type": data_type,
+                        "isnull": is_nullable,
+                    }
+                )
+                # print(f"Column Name: {column_name}, Data Type: {data_type}, Nullable: {is_nullable}")
+
         return table
-        
-            
-    def insert_data(self, table_name, data):
+
+    def insert_data(self, table_name:str, data:tuple) -> None:
         """Insert data into a table.
-        
+
         Args:
             table_name (str): Name of the table.
             data (tuple): Data to insert.
         """
         try:
             # Insert data into the table
-            placeholders = ', '.join(['?'] * len(data))
+            placeholders = ", ".join(["?"] * len(data))
             insert_query = f"INSERT INTO {table_name} VALUES ({placeholders});"
             self.cursor.execute(insert_query, data)
             self.conn.commit()
@@ -126,9 +130,9 @@ class SQLiteconn:
         except sqlite3.Error as e:
             print(f"Error inserting data: {e}")
 
-    def read_data(self, table_name):
+    def read_data(self, table_name:str) -> None:
         """Read data from a table.
-        
+
         Args:
             table_name (str): Name of the table.
         """
@@ -145,9 +149,11 @@ class SQLiteconn:
         except sqlite3.Error as e:
             print(f"Error reading data: {e}")
 
-    def update_data(self, table_name, column, new_value, condition_column, condition_value):
+    def update_data(
+        self, table_name:str, column:str, new_value:str, condition_column:str, condition_value:str
+    ) -> None:
         """Update data in a table based on a condition.
-        
+
         Args:
             table_name (str): Name of the table.
             column (str): Column to update.
@@ -157,16 +163,18 @@ class SQLiteconn:
         """
         try:
             # Update data in the table based on a condition
-            update_query = f"UPDATE {table_name} SET {column} = ? WHERE {condition_column} = ?;"
+            update_query = (
+                f"UPDATE {table_name} SET {column} = ? WHERE {condition_column} = ?;"
+            )
             self.cursor.execute(update_query, (new_value, condition_value))
             self.conn.commit()
             print("Data updated successfully.")
         except sqlite3.Error as e:
             print(f"Error updating data: {e}")
 
-    def delete_data(self, table_name, condition_column, condition_value):
+    def delete_data(self, table_name:str, condition_column:str, condition_value:str) -> None:
         """Delete data from a table based on a condition.
-        
+
         Args:
             table_name (str): Name of the table.
             condition_column (str): Column to use in the WHERE clause.
@@ -181,21 +189,20 @@ class SQLiteconn:
         except sqlite3.Error as e:
             print(f"Error deleting data: {e}")
 
-    def close_connection(self):
+    def close_connection(self) -> None:
         """Close the database connection."""
         self.conn.close()
         print("Database connection closed.")
-        
-        
-    def formatted_print(self,table_name):
+
+    def formatted_print(self, table_name:str) -> PrettyTable:
         """Print the contents of a table in a formatted manner.
-        
+
         Args:
             table_name (str): Name of the table.
         """
         self.cursor.execute(f"SELECT * FROM {table_name};")
         table_info = self.cursor.fetchall()
-        
+
         table_info_list = []
 
         table = PrettyTable()
@@ -204,9 +211,9 @@ class SQLiteconn:
 
         return table
 
-    def table_exists(self, table_name,db_path=None):
+    def table_exists(self, table_name:str, db_path:str|None=None) -> bool:
         """Check if a table exists.
-        
+
         Args:
             table_name (str): Name of the table.
             db_path (str, optional): Path to the database. Defaults to the initialized db_path.
@@ -220,11 +227,12 @@ class SQLiteconn:
             different_conn = True
             conn = self.conn
             cursor = self.conn.cursor()
-            
-        try:
 
+        try:
             # Query the sqlite_master table to check if the table exists
-            cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+            cursor.execute(
+                f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';"
+            )
             result = cursor.fetchone()
 
             # If result is not None, the table exists
@@ -238,16 +246,13 @@ class SQLiteconn:
             if different_conn:
                 conn.close()
 
-
-
-    def drop_table(self,table_name):
+    def drop_table(self, table_name:str) -> bool:
         """Drop a table by its name.
-        
+
         Args:
             table_name (str): Name of the table to drop.
         """
         try:
-
             # Drop the table if it exists
             self.cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
 
@@ -262,12 +267,11 @@ class SQLiteconn:
 
 # Example usage:
 if __name__ == "__main__":
-
     db_name = "students.sqlite"
     conn = SQLiteconn(db_name)
-    
+
     # Define table schema
-    table_name = "students"   
+    table_name = "students"
     columns = ["id TEXT", "name TEXT", "age INTEGER"]
 
     # # Create table
@@ -276,11 +280,11 @@ if __name__ == "__main__":
     # Insert data
     data = ("1", "Alice", 25)
     conn.insert_data(table_name, data)
-    
-    data = ("2","Bob", 23)
+
+    data = ("2", "Bob", 23)
     conn.insert_data(table_name, data)
-    
-    data = ("3","Charlie", 11)
+
+    data = ("3", "Charlie", 11)
     conn.insert_data(table_name, data)
 
     # Read data
@@ -292,5 +296,5 @@ if __name__ == "__main__":
     # Delete data
     conn.delete_data(table_name, "name", "Alice")
 
-    #Close the database connection
+    # Close the database connection
     conn.close_connection()
