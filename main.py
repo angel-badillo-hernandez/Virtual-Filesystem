b635db0036b480ad9ee343a242e2ca89b110acda
Angel Badillo Hernandez <badilloa022402@gmail.com>
2023-10-10 00:44:24 -0500

    minor changes

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from cmd_pkg import fileSystem
 from ParseCommand import parseCommand, ShellCommand
 
 # DB Constants
-DB_PATH:str = "filesystem.sqlite"
-CSV_FILE:str = "fakeFileData.csv"
-TABLE_NAME:str = "FileSystem"
+DB_PATH: str = "filesystem.sqlite"
+CSV_FILE: str = "fakeFileData.csv"
+TABLE_NAME: str = "FileSystem"
 
 # ANSI Escape Sequences for controlling text colors
-RESET = "\033[0m"      # Reset text formatting and color
-BOLD = "\033[1m"       # Bold text
-GREEN = "\033[92m"     # Green text
-YELLOW = "\033[93m"    # Yellow text
-CYAN = "\033[36m"      # Blue text
-PURPLE = "\033[35m"    # Purple text
+RESET = "\033[0m"  # Reset text formatting and color
+BOLD = "\033[1m"  # Bold text
+GREEN = "\033[92m"  # Green text
+YELLOW = "\033[93m"  # Yellow text
+CYAN = "\033[36m"  # Blue text
+PURPLE = "\033[35m"  # Purple text
 
-def prompt()->str:
+
+def prompt() -> str:
     """
     Returns a linux-like prompt.
     """
@@ -23,24 +24,27 @@ def prompt()->str:
 
     return f"{directory}{GREEN+BOLD}$: {RESET}"
 
+
 if __name__ == "__main__":
     fileSystem.set_db_path(DB_PATH)
     fileSystem.set_table_name(TABLE_NAME)
     fileSystem.csv_to_table(CSV_FILE)
-    
-    cont:str = "y"
-    
+
+    cont: str = "y"
+
     while cont != "n":
         cmdStr = input("Command: ")
 
         # Singular, "simple" command is parsed
-        shellCmd:ShellCommand = parseCommand(cmdStr).pop()
-    
+        shellCmd: ShellCommand = parseCommand(cmdStr).pop()
+
         if callable(getattr(cmd_pkg, shellCmd.name, None)):
             commandFunc = getattr(cmd_pkg, shellCmd.name)
-            result = commandFunc(flags=shellCmd.flags, \
-                                params=shellCmd.params, \
-                                stdin= shellCmd.stdin, \
-                                stdout=shellCmd.stdout)
-        print(result)
+            result = commandFunc(
+                flags=shellCmd.flags,
+                params=shellCmd.params,
+                stdin=shellCmd.stdin,
+                stdout=shellCmd.stdout,
+            )
+            print(result)
         cont = input("Continue? [y/n]: ")
